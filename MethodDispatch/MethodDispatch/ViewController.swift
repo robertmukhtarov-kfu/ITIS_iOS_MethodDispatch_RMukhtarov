e69a264a065a26135af182b385419141d65cecce
robertmukhtarov-kfu <rrmukhtarov@stud.kpfu.ru>
2021-09-20 18:48:02 +0300

    Initial commit

---
+++ MethodDispatch/MethodDispatch/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MethodDispatch
+//
+//  Created by RRMukhtarov on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
